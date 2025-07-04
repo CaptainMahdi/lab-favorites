3d040ca8179152da5b9809704e99ec59bbf7209d
CaptainMahdi <mahdi.hteit@gmail.com>
2025-07-04 16:38:11 -0500

    main non stretch

---
+++ main.py
@@ -0,0 +1,40 @@
+favthings = {"color": "red", "food": "Burger", "movie": "Click"}
+
+#catagory = input("Which catagory do you want to access, Color, Food, or Movies: ").lower()
+
+    
+more = False
+
+
+
+while more == False:
+    print("The catagories you have are: ")
+    for key in favthings.keys():
+        print(key)
+    print(" \n")
+    catagory = input("Which catagory do you want to access: ").lower()    
+
+    if catagory in favthings:
+        favorite_item = favthings[catagory]
+        print(f"My favorite {catagory} is {favorite_item}!")
+    else:
+        print("Invalid category!")
+
+        
+    addnewcat = input("Would you like to add a new favorite catagories list? (y/n) ")
+    if addnewcat == "y":
+        newcatname = input("Enter new catagory name: ")
+        nameincat = input(f"Enter your favorite for {newcatname}: ")
+        favthings[newcatname] = nameincat
+    else:
+        print("Ok!")
+
+    yn = input("Do you want to see another catagory?: (Y/n)").lower()
+    
+  
+    if yn == "y":
+        more = False
+    else: 
+        more = True    
+
+print("All done, Thanks for using!")
