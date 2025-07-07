ea55660289016346b391372050feae34169fef21
CaptainMahdi <mahdi.hteit@gmail.com>
2025-07-06 22:31:51 -0500

    refactord and refactord stretch

--- refactordstretch.py
+++ refactordstretch.py
@@ -14,49 +14,50 @@ def main():
     else:
         print("No saved favorite things list found. Starting fresh.")
 
-    def lookcats():
+    def lookcats(favs):
         cat = input("\nWhich category do you want to access?(Type none for no category) ").lower()
-        if cat in favthings:
-            print(f"My favorite {cat} is {favthings[cat]}!")
+        if cat in favs:
+            print(f"My favorite {cat} is {favs[cat]}!")
         elif cat == "none":
             print("Alright then!")
         else:
             print("Invalid category!")
-    def showcats():
-        if favthings == {}:
+    def showcats(favs):
+        if favs == {}:
             print("Nothing in your list.")
         else:
             print("\nThe categories you have are:")
-            for key in favthings.keys():
+            for key in favs.keys():
                 print(f"- {key}")
-    def addcats():
+    def add_favorite(favs):
         if input("Add a new category? (y/n): ").lower() == "y":
             new_category = input("New category name: ").strip().lower()
             new_value = input(f"Your favorite {new_category}: ").strip()
-            favthings[new_category] = new_value
-    def changecats():
+            favs[new_category] = new_value
+    def changecats(favs):
         if input("Make a change to a category? (y/n): ").lower() == "y":
             changecat = input("Which category to change? ").strip().lower()
-            if changecat in favthings:
+            if changecat in favs:
                 newval = input(f"New favorite for {changecat}: ").strip()
-            favthings[changecat] = newval
-        else:
-            print("That category doesn't exist.")
-    def delcats():
+                favs[changecat] = newval
+            else:
+                print("That category doesn't exist.")
+    def delcats(favs):
         if input("Delete a category? (y/n): ").lower() == "y":
             delcat = input("Which category to delete? ").strip().lower()
-            if delcat in favthings:
-                del favthings[delcat]
+            if delcat in favs:
+                del favs[delcat]
                 print(f"{delcat} deleted.")
             else:
                 print("That category doesn't exist.")
 
     while True:
-        showcats()
-        lookcats()
-        addcats()
-        changecats()
-        delcats()
+
+        showcats(favthings)
+        lookcats(favthings)
+        add_favorite(favthings)
+        changecats(favthings)
+        delcats(favthings)
 
         if input("Do you want to continue? (y/n): ").lower() != "y":
             break
